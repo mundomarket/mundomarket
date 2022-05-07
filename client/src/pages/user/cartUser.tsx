5cdfa327cdb8f79c2d2946f95a03494063368d43
yanfranve <franklinyanez6198@gmail.com>
2022-05-07 17:59:57 -0500

    cambios paypal

--- client/src/pages/user/cartUser.tsx
+++ client/src/pages/user/cartUser.tsx
@@ -1,14 +1,22 @@
-import {Box, Button, Card, CardContent, Divider, Grid,Typography} from '@mui/material';
-import { useParams } from 'react-router-dom';
-import { CartList, OrderSumary } from '../../components/cart';
-import NavBar from '../../components/ui/NavBar/NavBar'
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { useParams } from "react-router-dom";
+import { CartList, OrderSumary } from "../../components/cart";
+import NavBar from "../../components/ui/NavBar/NavBar";
+import { PayPalButtons } from "@paypal/react-paypal-js";
 //import { ShopLayout } from '../../components/layouts';
 
-const CartUser=()=>{
+const CartUser = () => {
+  //ME TRAIGO LOS PRODUCTOS DEL USUARIO
 
-//ME TRAIGO LOS PRODUCTOS DEL USUARIO
-
-/*
+  /*
 const {id} = useParams()
 const dispatch=useAppDispatch()
 useEffect(()=>{
@@ -17,44 +25,37 @@ useEffect(()=>{
 const product=useSelector((State:RootState) => State.rootReducer.detail); 
 */
 
+  return (
+    <>
+      <NavBar />
+      <Typography variant="h1" component="h1">
+        {" "}
+        Carrito
+      </Typography>
 
-    return(
-        <>
-            <NavBar/>
-            <Typography variant='h1' component='h1'> Carrito</Typography>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList editable />
+        </Grid>
 
-            <Grid container>
-                <Grid item xs={12} sm={7}>
-                    <CartList editable/>
-                </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Orden</Typography>
+              <Divider sx={{ my: 1 }} />
 
-                <Grid item xs={12} sm={5}>
-                    <Card className='summary-card'>
-                        <CardContent>
-                            <Typography variant='h2'>Orden</Typography>
-                            <Divider sx={{my:1}}/>
-
-                            <OrderSumary/>
-                            <Box sx={{mt:3}}>
-                                <Button color='secondary' className='circular-btn' fullWidth>
-                                    Checkout
-                                </Button>
-
-                            </Box>
-
-                            
-                        </CardContent>
-                        
-                    </Card>
-                </Grid>
-
-            </Grid>
-
-            
-
- 
-       </>
-    )
-}
+              <OrderSumary />
+              <Box sx={{ mt: 3 }}>
+                <Button color="secondary" className="circular-btn" fullWidth>
+                  <PayPalButtons />
+                </Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default CartUser;
