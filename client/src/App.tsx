4a507149d9a60aab8bdc6296281368c7bca03c38
yanfranve <franklinyanez6198@gmail.com>
2022-05-09 11:58:47 -0500

    ajuste paypal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,53 +1,45 @@
-import React from 'react';
-import './App.css';
-import {Route,Routes} from 'react-router-dom'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import '../src/styles/globals.css'
-import { lightTheme } from './themes/light-theme';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import "../src/styles/globals.css";
+import { lightTheme } from "./themes/light-theme";
 
-import Landing from './pages/landing';
-import Home from './pages/home'
-import Product from './pages/product/details'
-import Summary from './pages/checkout/summary'
-import History from './pages/orders/history'
-import Cart from './pages/cart'
-import Login from './pages/auth/login'
-import Register from './pages/auth/register'
-import CrearProducto from './pages/formNuevoProdcto'
-import Profile from './pages/profile';
-import CartUser from './pages/user/cartUser'
+import Landing from "./pages/landing";
+import Home from "./pages/home";
+import Product from "./pages/product/details";
+import Summary from "./pages/checkout/summary";
+import History from "./pages/orders/history";
+import Cart from "./pages/cart";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import CrearProducto from "./pages/formNuevoProdcto";
+import Profile from "./pages/profile";
+import CartUser from "./pages/user/cartUser";
 
-import Prueba from './pages/product/Recommended'
-
-
-
+import Prueba from "./pages/product/Recommended";
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={lightTheme}>
-      <CssBaseline/>
-      <Routes>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={Landing()} />
+          <Route path="/home" element={Home()} />
+          <Route path="/product/:id" element={<Product />} />
 
-        <Route path='/' element={Landing()}/>
-        <Route path='/home' element={Home()}/>
-        <Route path='/product/:id' element={<Product/>}/>
+          <Route path="/user/:id" element={<CartUser />} />
+          <Route path="/summary" element={Summary()} />
+          <Route path="/history" element={History()} />
+          <Route path="/cart" element={Cart()} />
+          <Route path="/login" element={Login()} />
+          <Route path="/register" element={Register()} />
+          <Route path="/crearproducto" element={CrearProducto()} />
+          <Route path="/profile" element={<Profile />} />
 
-
-        <Route path='/user/:id' element={<CartUser/>}/>
-        <Route path='/summary' element={Summary()}/>
-        <Route path='/history' element={History()}/>
-        <Route path='/cart' element={Cart()}/>
-        <Route path='/login' element={Login()}/>
-        <Route path='/register' element={Register()}/>
-        <Route path='/crearproducto' element={CrearProducto()}/>
-        <Route path='/profile' element={<Profile/>}/>
-
-        <Route path='/prueba' element={<Prueba/>}/>
-
-
-
-      </Routes>
+          <Route path="/prueba" element={<Prueba />} />
+        </Routes>
       </ThemeProvider>
     </div>
   );
--- client/src/pages/orders/[id].tsx
+++ client/src/pages/orders/[id].tsx
@@ -1,16 +1,33 @@
-import { Box, Button, Card, CardContent, Divider, Grid, Typography, Link, Chip } from '@mui/material';
-import { CartList, OrderSumary } from '../../components/cart';
+import {
+  Box,
+  
+  Card,
+  CardContent,
+  Divider,
+  Grid,
+  Typography,
+  Link,
+  Chip,
+} from "@mui/material";
+import { CartList, OrderSumary } from "../../components/cart";
+import {  PayPalButtons } from "@paypal/react-paypal-js";
 //import NextLink from 'next/link'
 
 //import { ShopLayout } from '../../components/layouts';
-import { CreditCardOffOutlined, CreditScoreOutlined } from '@mui/icons-material';
+import {
+  CreditCardOffOutlined,
+  CreditScoreOutlined,
+} from "@mui/icons-material";
 
-const OrderPage=()=>{
-    return(
-        <>
-            <Typography variant='h1' component='h1'> Orden: 0516168</Typography>
+const OrderPage = () => {
+  return (
+    <>
+      <Typography variant="h1" component="h1">
+        {" "}
+        Orden: 0516168
+      </Typography>
 
-            {/*<Chip
+      {/*<Chip
                 sx={{my:2}}
                 label="pendiente de pago"
                 variant='outlined'
@@ -18,75 +35,81 @@ const OrderPage=()=>{
                 icon={ <CreditCardOffOutlined/>}
             />*/}
 
-            <Chip
-                sx={{my:2}}
-                label="La orden ya fue pagada"
-                variant='outlined'
-                color="success"
-                icon={ <CreditScoreOutlined/>}
-            />
+      <Chip
+        sx={{ my: 2 }}
+        label="La orden ya fue pagada"
+        variant="outlined"
+        color="success"
+        icon={<CreditScoreOutlined />}
+      />
 
-            <Grid container>
-                <Grid item xs={12} sm={7}>
-                    <CartList editable={false}/>
-                </Grid>
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList editable={false} />
+        </Grid>
 
-                <Grid item xs={12} sm={5}>
-                    <Card className='summary-card'>
-                        <CardContent>
-                            <Typography variant='h2'>Resumen</Typography>
-                            <Divider sx={{my:1}}/>
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Resumen</Typography>
+              <Divider sx={{ my: 1 }} />
 
-                            <Box display='flex' justifyContent='space-between'>
-                                <Typography variant='subtitle1'> Dirección de entrega</Typography>
-                               
-                                    <Link underline='always'>
-                                        Editar
-                                    </Link>
-       
-                            </Box>
+              <Box display="flex" justifyContent="space-between">
+                <Typography variant="subtitle1">
+                  {" "}
+                  Dirección de entrega
+                </Typography>
 
-                            
-                            <Typography>Gabriel Goliger</Typography>
-                            <Typography>Rambla gandhi 359</Typography>
-                            <Typography>Montevideo Uruguay</Typography>
-                            <Typography>099944268</Typography>
+                <Link underline="always">Editar</Link>
+              </Box>
 
-                            <Divider sx={{my:1}}/>
+              <Typography>Gabriel Goliger</Typography>
+              <Typography>Rambla gandhi 359</Typography>
+              <Typography>Montevideo Uruguay</Typography>
+              <Typography>099944268</Typography>
 
-                            <Box display='flex' justifyContent='end'>
-                                
-                                    <Link underline='always'>
-                                        Editar
-                                    </Link>
+              <Divider sx={{ my: 1 }} />
 
-                               
-                            </Box>
+              <Box display="flex" justifyContent="end">
+                <Link underline="always">Editar</Link>
+              </Box>
 
-                            <OrderSumary/>
+              <OrderSumary />
 
-                            <Box sx={{mt:3}}>
-                                <h1>Pagar</h1>
-                                <Chip
-                                    sx={{my:2}}
-                                    label="La orden ya fue pagada"
-                                    variant='outlined'
-                                    color="success"
-                                    icon={ <CreditScoreOutlined/>}
-                                />
-
-                            </Box>
-
-                            
-                        </CardContent>
-                        
-                    </Card>
-                </Grid>
-
-            </Grid>
-            </>
-
-    )
-}
+              <Box sx={{ mt: 3 }}>
+                <PayPalButtons
+                  createOrder={(data: any, actions: any) => {
+                    return actions.order.create({
+                      prchase_units: [
+                        {
+                          amo: {
+                            value: "1.99",
+                          },
+                        },
+                      ],
+                    });
+                  }}
+                  onApprove={(data: any, actions: any) => {
+                    return actions.order!.capture().then((details: any) => {
+                      const name = details.payer.name.given_name;
+                      alert(`Transaction completed by ${name}`);
+                    });
+                  }}
+                />
+                <Chip
+                  sx={{ my: 2 }}
+                  label="La orden ya fue pagada"
+                  variant="outlined"
+                  color="success"
+                  icon={<CreditScoreOutlined />}
+                />
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
 export default OrderPage;
