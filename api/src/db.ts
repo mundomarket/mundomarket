71f732dc44a5d2dac8f1a1cb43a1f84763c64046
mundomarket <henrymundomarket@gmail.com>
2022-05-10 01:26:19 -0400

    actualizacion del deploy en heroku

--- api/src/db.ts
+++ api/src/db.ts
@@ -6,7 +6,7 @@ import config from "./api.config"
 (async () => {
   try{
     // const mongooseOptions:ConnectionOptions ={
-    //   userUnifiedTypology:true
+    //   userUnifiedTypology:true;
     // }
       
     const db= await mongoose.connect(`mongodb+srv://mundomarket:${config.MONGO_PASSWORD}@cluster0.tqn5t.mongodb.net/${config.MONGO_DATABASE}?retryWrites=true&w=majority`,{
