a3e7ca0299e656422ee6cac1ea583ec7f5dc1577
leonelborgesdev <leonel03t@gmail.com>
2022-08-29 01:46:37 -0400

    mundomarket

--- api/src/config.ts
+++ api/src/config.ts
@@ -4,7 +4,7 @@ import 'dotenv/config'
 export default{
     MONGO_DATABASE:process.env.MONGO_DATABASE || "mundomarket",
     MONGO_USER:process.env.MONGO_USER || "mundomarket",
-    MONGO_PASSWORD:process.env.MONGO_PASSWORD || "leodax33558292",
+    MONGO_PASSWORD:process.env.MONGO_PASSWORD || "mundomarket",
     MONGO_HOST:process.env.MONGO_HOST || "localhost",
     SECRET_JWT : 'secret' 
 }
